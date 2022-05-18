ec258c8521ef1ee52052ae353b54b9ba6776c522
ricardo acebal <richardrcmark.909@gmail.com>
2022-05-18 12:50:56 -0300

    Primeras vistas

--- allcripto/views.py
+++ allcripto/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Bitcoin, BitcoinBody
 
-# Create your views here.
+
+def index(request):
+    return render(request, "allcripto/index.html")
+
+
+def bitcoin(request):
+    #cambiar mañana a '-id' para que las nuevas noticias se muestren arriba
+    bitcoin_list = Bitcoin.objects.all().order_by('id')
+    return render(request, "allcripto/bitcoin.html", {
+        "bitcoin_list" : bitcoin_list
+    })
+
+def bitcoin_link(request):
+    bitcoin_item = BitcoinBody.objects.get(pk='bitcoinbody_id')
+    return render(request, "allcripto/bitcoin_detail.html", {
+        "bitcoin_item" : bitcoin_item
+    })
