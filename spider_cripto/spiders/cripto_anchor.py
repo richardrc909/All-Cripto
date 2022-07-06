988d6fb2af40827218dcb280583cb0c67ce77661
ricardo acebal <richardrcmark.909@gmail.com>
2022-07-06 14:17:47 -0300

    cambio de nombres en spiders, lista de tags para start urls

--- spider_cripto/spiders/cripto_anchor.py
+++ spider_cripto/spiders/cripto_anchor.py
@@ -8,6 +8,7 @@ class BitcoinBody(Spider):
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36'
     }
     download_delay = 2
+    tags = ['bitcoin', 'ethereum', 'metaverse', '/news/price-analysis-', 'business']
     start_urls = ['https://es.cointelegraph.com/tags/ethereum']
 
     def parse(self, response):
